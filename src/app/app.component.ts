b15985f60045e4c9106abe24f45293a6e7e0b043
hugo <hugo.dev73@gmail.com>
2025-08-23 13:43:37 -0600

    add scss basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolbarComponent, MatSidenavModule],
+  imports: [RouterOutlet, ToolbarComponent, MatSidenavModule, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
